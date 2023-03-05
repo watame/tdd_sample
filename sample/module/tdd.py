0ee10edd22b47682cc3e135b03befb4c7b51d0e4
watame <pandora.border@gmail.com>
2023-03-05 21:26:02 +0900

    :+1: TDDテスト用の関数を作成

---
+++ sample/module/tdd.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf_8 -*-
+"""TDD練習用関数モジュール
+"""
+
+
+def fizzbuzz(number: int):
+    pass
---
+++ sample/test/test_tdd.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf_8 -*-
+"""TDD練習用関数テスト
+"""
